fd6c2462ebf9826a8271a48d20ef1dc7102f2478
夏影 <summer@summerlight.com>
2026-03-23 15:03:21 +0800

    🔧 修复 About 页面加载卡死问题，移除弹幕计数并异步加载媒体占用

--- resources/js/api/system.ts
+++ resources/js/api/system.ts
@@ -1,3 +1,7 @@
 export const getSystemInfo = () => {
     return fetch('/api/system/info').then((res) => res.json());
 };
+
+export const getMediaUsage = () => {
+    return fetch('/api/system/media-usage').then((res) => res.json());
+};
